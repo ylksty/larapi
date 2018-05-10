3a90628866d22f5b75387c1ee6416d02119ffd03
yang linkai <ylksty@163.com>
2018-05-10 22:54:08 +0800

    mod

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Route::pattern('id', '[0-9]+'); // 全局约束
 
         parent::boot();
     }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['check.age'])->group(function() {
+    Route::get('foo/{id?}/{age?}', function ($id = 'default', $age = 20) {
+        $url = route('nameTest', ['id' => 1]);
+        return 'Hello World'.
+            '<br />路由参数id:'.$id.
+            '<br />路由命名:'.$url.
+            '<br />中间件age:'.$age.
+            '<br />';
+    })->where('name', '[A-Za-z]+')->name('nameTest');
+});
